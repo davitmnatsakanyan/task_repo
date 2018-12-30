280242d226138ec679b1b0122a532a754fdc4c29
Davit <dav.mnatsakanyan@gmail.com>
2018-12-30 17:24:16 +0400

    hjdhdh

--- Models/DB.php
+++ Models/DB.php
@@ -17,12 +17,7 @@ class DB
             $username = conf('database.username');
             $password = conf('database.password');
 
-            echo $host.'<br>';
-            echo $dbname.'<br>';
-            echo $username.'<br>';
-            echo $password; die();
-
-            $this->pdo = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_4cf3ef7f247f1dd", "$username", "$password");
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", "$username", "$password");
         } catch (PDOException $e) {
             print "Error!: " . $e->getMessage() . "<br/>";
             die();
